5e2269695509e923b54776c5607d79601daf6ec7
fahimhassann <hassan.fahim2345@gmail.com>
2025-11-19 13:12:53 -0600

    Add polls endpoint + dashboard integration

--- src/api/api_router.py
+++ src/api/api_router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from src.api import authorization, calendar, settings
+from src.api import authorization, calendar, settings, polls
 
 """
 Container for all routing capabilities of the backend's API.
@@ -11,3 +11,4 @@ api_router = APIRouter()
 api_router.include_router(authorization.router, prefix="/auth", tags=["auth"])
 api_router.include_router(calendar.router, prefix="/calendar", tags=["calendar"])
 api_router.include_router(settings.router, prefix="/settings", tags=["settings"])
+api_router.include_router(polls.router, prefix="/polls", tags=["polls"])
